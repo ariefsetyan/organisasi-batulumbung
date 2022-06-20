03917d6949e4cdde03e9d96908b7f7cb21d0b656
windimasyundari <windimasyundarii@gmail.com>
2022-06-20 23:07:58 +0800

    ww

--- resources/views/pengurus/index.blade.php
+++ resources/views/pengurus/index.blade.php
@@ -33,7 +33,7 @@
   <div class="hero_area">
     <!-- header section strats -->
     <header class="header_section">
-      <div class="container">
+      <div class="container" id="home">
         <nav class="navbar navbar-expand-lg custom_nav-container pt-3">
           <a class="navbar-brand" href="#">
             <img src="{{asset('template')}}/plugins/images/logo-batulumbung.jpg" alt="" /><span>
@@ -44,9 +44,27 @@
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-        </nav>
-      </div>
-    </header>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
+              <ul class="navbar-nav  ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#home">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#pengumuman">Pengumuman</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#kegiatan">Kegiatan </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#event">Event</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </header>
     <!-- end header section -->
     <!-- slider section -->
     <section class=" slider_section position-relative">
@@ -61,13 +79,11 @@
                       <div class="slider_item-detail">
                         <div>
                           <h1>
-                           Selamat Datang
+                            Selamat Datang
+                            Di Organisasi Batulumbung
                           </h1><br/>
-                          <h2>
-                            Organisasi Banjar Batulumbung
-                          </h2>
                           <p>
-                            
+                            Menampilkan informasi mengenai Organisasi yang terdapat di Banjar Batulumbung
                           </p>
                           <div class="d-flex">
                             <a href="pengurus/login" class="text-uppercase custom_orange-btn mr-3">
@@ -80,7 +96,7 @@
                     <div class="col-md-6">
                       <div class="slider_img-box">
                         <div>
-                          <img src="images/slide-img.png" alt="" class="" />
+                            <img src="/template/plugins/images/img.jpeg" alt="" class="" /> 
                         </div>
                       </div>
                     </div>
@@ -98,22 +114,16 @@
                       <div class="slider_item-detail">
                         <div>
                           <h1>
-                            Welcome to <br />
-                            Our Fruits Shop
-                          </h1>
+                            Selamat Datang
+                            Di Organisasi Batulumbung
+                          </h1><br/>
                           <p>
-                            There are many variations of passages of Lorem
-                            Ipsum available, but the majority have suffered
-                            alteration in some form, by injected humour, or
-                            randomised words which don't look even slightly
-                            believable.
+                            Menampilkan informasi mengenai Organisasi yang terdapat di Banjar Batulumbung
                           </p>
+                      
                           <div class="d-flex">
-                            <a href="" class="text-uppercase custom_orange-btn mr-3">
-                              Shop Now
-                            </a>
-                            <a href="" class="text-uppercase custom_dark-btn">
-                              Contact Us
+                            <a href="pengurus/login" class="text-uppercase custom_orange-btn mr-3">
+                              LOGIN
                             </a>
                           </div>
                         </div>
@@ -122,7 +132,7 @@
                     <div class="col-md-6">
                       <div class="slider_img-box">
                         <div>
-                          <img src="images/slide-img.png" alt="" class="" />
+                            <img src="/template/plugins/images/img1.jpeg" alt="" class="" /> 
                         </div>
                       </div>
                     </div>
@@ -140,22 +150,15 @@
                       <div class="slider_item-detail">
                         <div>
                           <h1>
-                            Welcome to <br />
-                            Our Fruits Shop
-                          </h1>
+                            Selamat Datang
+                            Di Organisasi Batulumbung
+                          </h1><br/>
                           <p>
-                            There are many variations of passages of Lorem
-                            Ipsum available, but the majority have suffered
-                            alteration in some form, by injected humour, or
-                            randomised words which don't look even slightly
-                            believable.
+                            Menampilkan informasi mengenai Organisasi yang terdapat di Banjar Batulumbung
                           </p>
                           <div class="d-flex">
-                            <a href="" class="text-uppercase custom_orange-btn mr-3">
-                              Shop Now
-                            </a>
-                            <a href="" class="text-uppercase custom_dark-btn">
-                              Contact Us
+                            <a href="pengurus/login" class="text-uppercase custom_orange-btn mr-3">
+                              LOGIN
                             </a>
                           </div>
                         </div>
@@ -164,7 +167,7 @@
                     <div class="col-md-6">
                       <div class="slider_img-box">
                         <div>
-                          <img src="images/slide-img.png" alt="" class="" />
+                          <img src="/template/plugins/images/img2.jpeg" alt="" class="" /> 
                         </div>
                       </div>
                     </div>
@@ -188,48 +191,22 @@
     <!-- end slider section -->
   </div>
 
-  <!-- service section -->
+  <!-- pengumuman section -->
 
   <section class="service_section layout_padding ">
-    <div class="container">
+    <div class="container" id="pengumuman">
       <h2 class="custom_heading">Pengumuman</h2>
       <div class=" layout_padding2">
         <div class="card-deck">
           <div class="card">
            
-            <div class="card-body" style="color:white">
-                <h4 class="card-title" style="font-weight: 800; "></h4>
-                <p class="card-text"></p>
+            <div class="card-body">
+            <!--  -->
             </div>
           
           </div>
         </div>
-          <div class="card">
-            <img class="card-img-top" src="images/card-item-2.png" alt="Card image cap" />
-            <div class="card-body">
-              <h5 class="card-title">Grapes</h5>
-              <p class="card-text">
-                There are many variations of passages of Lorem Ipsum
-                available, but the majority have suffered alteration in some
-                form, by injected humour, or randomised words which don't look
-                even slightly believable.
-              </p>
-            </div>
-          </div>
-          <div class="card">
-            <img class="card-img-top" src="images/card-item-3.png" alt="Card image cap" />
-            <div class="card-body">
-              <h5 class="card-title">Gauva</h5>
-              <p class="card-text">
-                There are many variations of passages of Lorem Ipsum
-                available, but the majority have suffered alteration in some
-                form, by injected humour, or randomised words which don't look
-                even slightly believable.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+         
       <div class="d-flex justify-content-center">
         <a href="" class="custom_dark-btn">
           Read More
@@ -238,22 +215,45 @@
     </div>
   </section>
 
-  <!-- end service section -->
+  <!-- end pengumuman section -->
 
-  <!-- fruits section -->
+   <!-- kegiatan section -->
+
+   <section class="service_section layout_padding ">
+    <div class="container" id="kegiatan">
+      <h2 class="custom_heading">Kegiatan</h2>
+      <div class=" layout_padding2">
+        <div class="card-deck">
+          <div class="card">
+           
+            <div class="card-body">
+            <!--  -->
+            </div>
+          
+          </div>
+        </div>
+         
+      <div class="d-flex justify-content-center">
+        <a href="" class="custom_dark-btn">
+          Read More
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <!-- end kegiatan section -->
+
+  <!-- event section -->
 
   <section class="fruit_section">
-    <div class="container">
-      <h2 class="custom_heading">Kegiatan</h2>
-      <p class="custom_heading-text">
-        There are many variations of passages of Lorem Ipsum available, but
-        the majority have
-      </p>
+    <div class="container" id="event">
+      <h2 class="custom_heading">Event</h2>
       <div class="row layout_padding2">
         <div class="col-md-8">
+          
           <div class="fruit_detail-box">
             <h3>
-              Best Fresh Orange
+              
             </h3>
             <p class="mt-4 mb-5">
               but the majority have suffered alteration in some form, by
@@ -267,6 +267,7 @@
               </a>
             </div>
           </div>
+          
         </div>
         <div class="col-md-4 d-flex justify-content-center align-items-center">
           <div class="fruit_img-box d-flex justify-content-center align-items-center">
@@ -327,292 +328,7 @@
     </div>
   </section>
 
-  <!-- end fruits section -->
-
-  <!-- tasty section -->
-  <section class="tasty_section">
-    <div class="container_fluid">
-      <h2>
-        Very tasty fruits
-      </h2>
-    </div>
-  </section>
-
-  <!-- end tasty section -->
-
-  <!-- client section -->
-
-  <section class="client_section layout_padding">
-    <div class="container">
-      <h2 class="custom_heading">Testimonial</h2>
-      <p class="custom_heading-text">
-        There are many variations of passages of Lorem Ipsum available, but
-        the majority have
-      </p>
-      <div>
-        <div id="carouselExampleControls-2" class="carousel slide" data-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <div class="client_container layout_padding2">
-                <div class="client_img-box">
-                  <img src="images/client.png" alt="" />
-                </div>
-                <div class="client_detail">
-                  <h3>
-                    Johnhex
-                  </h3>
-                  <p class="custom_heading-text">
-                    There are many variations of passages of Lorem Ipsum
-                    available, but the majority have suffered alteration in
-                    some form, by injected humour, or randomised words which
-                    don't look even slightly believable. If you are <br />
-                    going to use a passage of Lorem Ipsum, you need to be sure
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <div class="client_container layout_padding2">
-                <div class="client_img-box">
-                  <img src="images/client.png" alt="" />
-                </div>
-                <div class="client_detail">
-                  <h3>
-                    Johnhex
-                  </h3>
-                  <p class="custom_heading-text">
-                    There are many variations of passages of Lorem Ipsum
-                    available, but the majority have suffered alteration in
-                    some form, by injected humour, or randomised words which
-                    don't look even slightly believable. If you are <br />
-                    going to use a passage of Lorem Ipsum, you need to be sure
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <div class="client_container layout_padding2">
-                <div class="client_img-box">
-                  <img src="images/client.png" alt="" />
-                </div>
-                <div class="client_detail">
-                  <h3>
-                    Johnhex
-                  </h3>
-                  <p class="custom_heading-text">
-                    There are many variations of passages of Lorem Ipsum
-                    available, but the majority have suffered alteration in
-                    some form, by injected humour, or randomised words which
-                    don't look even slightly believable. If you are <br />
-                    going to use a passage of Lorem Ipsum, you need to be sure
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="custom_carousel-control">
-            <a class="carousel-control-prev" href="#carouselExampleControls-2" role="button" data-slide="prev">
-              <span class="" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls-2" role="button" data-slide="next">
-              <span class="" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
-
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- end client section -->
-
-  <!-- contact section -->
-  <section class="contact_section layout_padding">
-    <div class="container">
-      <h2 class="font-weight-bold">
-        Contact Us
-      </h2>
-      <div class="row">
-        <div class="col-md-8 mr-auto">
-          <form action="">
-            <div class="contact_form-container">
-              <div>
-                <div>
-                  <input type="text" placeholder="Name">
-                </div>
-                <div>
-                  <input type="text" placeholder="Phone Number">
-                </div>
-                <div>
-                  <input type="email" placeholder="Email">
-                </div>
-
-                <div class="mt-5">
-                  <input type="text" placeholder="Message">
-                </div>
-                <div class="mt-5">
-                  <button type="submit">
-                    send
-                  </button>
-                </div>
-              </div>
-
-            </div>
-
-          </form>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- end contact section -->
-
-
-  <!-- map section -->
-  <section class="map_section">
-    <div id="map" class="h-100 w-100 ">
-    </div>
-  </section>
-
-  <!-- end map section -->
-
-  <section class="info_section layout_padding">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-3">
-          <h5>
-            Fruits
-          </h5>
-          <ul>
-            <li>
-              randomised
-            </li>
-            <li>
-              words which
-            </li>
-            <li>
-              don't look even
-            </li>
-            <li>
-              slightly
-            </li>
-            <li>
-              believable. If you
-            </li>
-            <li>
-              are going to use
-            </li>
-            <li>
-              a passage of
-            </li>
-            <li>
-              Lorem Ipsum,
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-3">
-          <h5>
-            Services
-          </h5>
-          <ul>
-            <li>
-              randomised
-            </li>
-            <li>
-              words which
-            </li>
-            <li>
-              don't look even
-            </li>
-            <li>
-              slightly
-            </li>
-            <li>
-              believable. If you
-            </li>
-            <li>
-              are going to use
-            </li>
-            <li>
-              a passage of
-            </li>
-            <li>
-              Lorem Ipsum,
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-3">
-          <h5>
-            List
-          </h5>
-          <ul>
-            <li>
-              randomised
-            </li>
-            <li>
-              words which
-            </li>
-            <li>
-              don't look even
-            </li>
-            <li>
-              slightly
-            </li>
-            <li>
-              believable. If you
-            </li>
-            <li>
-              are going to use
-            </li>
-            <li>
-              a passage of
-            </li>
-            <li>
-              Lorem Ipsum,
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-3">
-          <div class="social_container">
-            <h5>
-              Follow Us
-            </h5>
-            <div class="social-box">
-              <a href="">
-                <img src="images/fb.png" alt="">
-              </a>
-
-              <a href="">
-                <img src="images/twitter.png" alt="">
-              </a>
-              <a href="">
-                <img src="images/linkedin.png" alt="">
-              </a>
-              <a href="">
-                <img src="images/instagram.png" alt="">
-              </a>
-            </div>
-          </div>
-          <div class="subscribe_container">
-            <h5>
-              Subscribe Now
-            </h5>
-            <div class="form_container">
-              <form action="">
-                <input type="email">
-                <button type="submit">
-                  Subscribe
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-
+  <!-- end event section -->
 
   <!-- footer section -->
   <section class="container-fluid footer_section">
@@ -623,8 +339,8 @@
   </section>
   <!-- footer section -->
 
-  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
-  <script type="text/javascript" src="js/bootstrap.js"></script>
+  <script type="text/javascript" src="template/js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="template/js/bootstrap.js"></script>
 
   <script>
     // This example adds a marker to indicate the position of Bondi Beach in Sydney,
